c27f1095cf1c4abe55df0d09d51a60331f74c93d
moabdels <mkabdelsater@gmail.com>
2025-07-04 16:59:29 +0300

    Add some basic defines to philo.h

---
+++ philo/inc/philo.h
@@ -0,0 +1,26 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <stdbool.h>
+
+# define PHILO_CAP 200
+# define STR_PHILO_CAP "200"
+
+# ifndef DEBUG_FORMATTING
+#  define DEBUG_FORMATTING 0
+# endif
+
+# define NC		"\e[0m"
+# define RED	"\e[31m"
+# define GREEN	"\e[32m"
+# define PURPLE	"\e[35m"
+# define CYAN	"\e[36m"
+
+
+#endif
