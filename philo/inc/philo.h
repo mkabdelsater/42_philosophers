95d0dd2660283a20c99de003584ab7b269394265
moabdels <mkabdelsater@gmail.com>
2025-07-04 17:31:40 +0300

    Create the basic structs needed for the program

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -22,5 +22,33 @@
 # define PURPLE	"\e[35m"
 # define CYAN	"\e[36m"
 
+typedef struct s_philosopher	t_philosopher;
+
+typedef struct s_philo
+{
+	unsigned int	philo_count;
+	time_t			start_time;
+	time_t			til_death;
+	time_t			til_meal;
+	time_t			til_sleep;
+	int				must_eat_count;
+	bool			sim_stop;
+	pthread_mutex_t	sim_stop_lock;
+	pthread_mutex_t	write_lock;
+	pthread_mutex_t	*fork_locks;
+	t_philosopher	**philosophers;
+	pthread_t		reaper;
+}	t_philo;
+
+typedef struct s_philosopher
+{
+	pthread_t			thread;
+	unsigned int		id;
+	unsigned int		meals_had;
+	unsigned int		fork[2];
+	pthread_mutex_t		meal_time_lock;
+	time_t				since_last_meal;
+	t_philo				*table;
+}	t_philosopher;
 
 #endif
---
+++ philo/src/main.c
@@ -0,0 +1,8 @@
+#include "philo.h"
+
+int	main(int ac, char **av)
+{
+	t_philo	*table;
+
+	table = NULL;
+}
